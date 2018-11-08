d8338a29c1c386b1fa4f6dd13fb3f670d6ec3314
David Chen <davidchen9568@gmail.com>
2018-11-08 15:06:25 +0800

    Add ability to read from a string or lines of strings.

--- lvm_read.py
+++ lvm_read.py
@@ -70,16 +70,27 @@ def _read_lvm_base(filename):
     :param filename: filename of the lvm file
     :return lvm_data: lvm dict
     """
+    f = open(filename, 'r')
+    lvm_data = read_lines(f)
+    f.close()
+    return lvm_data
+
+
+def read_lines(lines):
+    """ Read lines of strings.
+
+    :param lines: lines of the lvm file
+    :return lvm_data: lvm dict
+    """
     lvm_data = dict()
     lvm_data['Decimal_Separator'] = '.'
-    f = open(filename, 'r')
     data_channels_comment_reading = False
     data_reading = False
     segment = None
     first_column = 0
     nr_of_columns = 0
     segment_nr = 0
-    for line in f:
+    for line in lines:
         line = line.replace('\r', '')
         line_sp = line.replace('\n', '').split('\t')
         if line_sp[0] in ['***End_of_Header***', 'LabVIEW Measurement']:
@@ -91,19 +102,19 @@ def _read_lvm_base(filename):
             data_reading = False
             segment_nr += 1
             continue
-        elif data_reading:#this was moved up, to speed up the reading
+        elif data_reading:  # this was moved up, to speed up the reading
             seg_data.append([float(a.replace(lvm_data['Decimal_Separator'], '.') if a else 'NaN') for a in
                              line_sp[first_column:(nr_of_columns + 1)]])
-        elif segment==None:
+        elif segment == None:
             if len(line_sp) is 2:
                 key, value = line_sp
                 lvm_data[key] = value
-        elif segment!=None:
+        elif segment != None:
             if line_sp[0] == 'Channels':
                 key, value = line_sp[:2]
-                nr_of_columns = len(line_sp)-1
+                nr_of_columns = len(line_sp) - 1
                 segment[key] = eval(value)
-                if nr_of_columns<segment['Channels']:
+                if nr_of_columns < segment['Channels']:
                     nr_of_columns = segment['Channels']
                 data_channels_comment_reading = True
             elif line_sp[0] == 'X_Value':
@@ -117,21 +128,42 @@ def _read_lvm_base(filename):
             elif data_channels_comment_reading:
                 key, values = line_sp[0], line_sp[1:(nr_of_columns + 1)]
                 if key in ['Delta_X', 'X0', 'Samples']:
-                    segment[key] = [eval(val.replace(lvm_data['Decimal_Separator'], '.')) if val else np.nan for val in values]
+                    segment[key] = [eval(val.replace(lvm_data['Decimal_Separator'], '.')) if val else np.nan for val in
+                                    values]
                 else:
                     segment[key] = values
             elif len(line_sp) is 2:
                 key, value = line_sp
                 segment[key] = value
 
-    if not lvm_data[segment_nr-1]:
-        del lvm_data[segment_nr-1]
+    if not lvm_data[segment_nr - 1]:
+        del lvm_data[segment_nr - 1]
         segment_nr -= 1
     lvm_data['Segments'] = segment_nr
     for s in range(segment_nr):
         lvm_data[s]['data'] = np.asarray(lvm_data[s]['data'])
-    f.close()
     return lvm_data
+
+
+def read_str(str):
+    """
+    Parse the string as the content of lvm file.
+
+    :param str:   input string
+    :return:      dictionary with lvm data
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> import urllib
+    >>> filename = 'short.lvm' #download a sample file from github
+    >>> sample_file = urllib.request.urlopen('https://raw.githubusercontent.com/openmodal/lvm_read/master/data/'+filename).read()
+    >>> str = sample_file.decode('utf-8') # convert to string
+    >>> lvm = lvm_read.read_str(str) #read the string as lvm file content
+    >>> lvm.keys() #explore the dictionary
+    dict_keys(['', 'Date', 'X_Columns', 'Time_Pref', 'Time', 'Writer_Version',...
+    """
+    return read_lines(str.splitlines(keepends=True))
 
 
 def read(filename, read_from_pickle=True, dump_file=True):
