a20623c1ad81f765e65cd28c50e60844b1289797
Janko Slavič <janko.slavic@gmail.com>
2023-07-01 07:21:25 +0200

    updated setup for v1.21 release

--- setup.py
+++ setup.py
@@ -1,27 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Copyright (C) 2014-2017 Matjaž Mršnik, Miha Pirnat, Janko Slavič, Blaž Starc (in alphabetic order)
-#
-# This file is part of lvm_read.
-#
-# lvm_read is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3 of the License.
-#
-# lvm_read is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with lvm_read.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
 desc = """\
 LabView Measurement File Reader
 
-For a showcase see: https://github.com/openmodal/lvm_read/blob/master/Showcase%20lvm_read.ipynb
+For a showcase see: https://github.com/ladisk/lvm_read/blob/master/Showcase%20lvm_read.ipynb
 See also specifications: http://www.ni.com/tutorial/4139/en/
 =============
 
@@ -31,10 +11,10 @@ A simple module for reading the LabView LVM text file.
 #from distutils.core import setup, Extension
 from setuptools import setup, Extension
 setup(name='lvm_read',
-      version='1.20',
+      version='1.21',
       author='Janko Slavič et al.',
       author_email='janko.slavic@fs.uni-lj.si',
-      url='https://github.com/openmodal/lvm_read',
+      url='https://github.com/ladisk/lvm_read',
       py_modules=['lvm_read'],
       #ext_modules=[Extension('lvm_read', ['data/short.lvm'])],
       long_description=desc,
