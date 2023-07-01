015d7e3df50a319adcb362e25151c625857d9c3a
Janko Slavič <janko.slavic@gmail.com>
2023-07-01 07:05:16 +0200

    changed licence, small polishing

--- lvm_read.py
+++ lvm_read.py
@@ -1,26 +1,5 @@
-# -*- coding: utf-8 -*-
-
-# Copyright (C) 2014-2017 Matjaž Mršnik, Miha Pirnat, Janko Slavič, Blaž Starc (in alphabetic order)
-#
-# This file is part of lvm_read.
-#
-# lvm_read is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3 of the License.
-#
-# lvm_read is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with lvm_read.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
 """
-This module is part of the www.openmodal.com project and is used for the 
-reading LabView Measurement File
+This module is used for the reading LabView Measurement File
 
 Author: Janko Slavič et al. (janko.slavic@fs.uni-lj.si)
 """
@@ -28,7 +7,7 @@ from os import path
 import pickle
 import numpy as np
 
-__version__ = '1.20'
+__version__ = '1.21'
 
 def _lvm_pickle(filename):
     """ Reads pickle file (for local use)
